cf0ce3a9fd6d786616a8cbbf601e74b540f01dc0
Maksym Davydenko <maksydenko@gmail.com>
2025-04-05 22:52:07 +0300

    Add manifest.ts file for web app configuration

---
+++ front/app/manifest.ts
@@ -0,0 +1,30 @@
+import { MetadataRoute } from "next";
+
+import { Pathname } from "@/shared/model";
+
+const manifest = async (): Promise<MetadataRoute.Manifest> => {
+  return {
+    background_color: "#fff",
+    display: "standalone",
+    icons: [
+      {
+        purpose: "maskable",
+        sizes: "192x192",
+        src: "/manifest/web-app-manifest-192x192.png",
+        type: "image/png",
+      },
+      {
+        purpose: "maskable",
+        sizes: "512x512",
+        src: "/manifest/web-app-manifest-512x512.png",
+        type: "image/png",
+      },
+    ],
+    name: "TagSync",
+    short_name: "TagSync",
+    start_url: Pathname.Home,
+    theme_color: "#fff",
+  };
+};
+
+export default manifest;
