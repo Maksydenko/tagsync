c1c532d1bc79e3510cad6c40ec87aa3eff9ebaf7
Maksym Davydenko <maksydenko@gmail.com>
2025-06-04 23:49:36 +0300

    Refactor Img component to simplify props

--- front/src/shared/ui/img/Img.tsx
+++ front/src/shared/ui/img/Img.tsx
@@ -1,70 +1,56 @@
 "use client";
 
-import { CSSProperties, FC, ReactNode, useRef, useState } from "react";
-import Image from "next/image";
-import { StaticImageData } from "next/image";
+import { FC, ReactNode, useRef, useState } from "react";
+import Image, { ImageProps } from "next/image";
 import { clsx } from "clsx";
 
 import s from "./Img.module.scss";
 
-interface ImgProps {
+interface ImgProps extends Omit<ImageProps, "alt"> {
   alt?: string;
   className?: string;
-  height?: number;
-  isFill?: boolean;
-  isPriority?: boolean;
+  customLoader?: ReactNode;
   isSvg?: boolean;
-  loader?: ReactNode;
   quality?: number;
-  sizes?: string;
-  src?: StaticImageData | string;
-  style?: CSSProperties;
-  width?: number;
 }
 
 export const Img: FC<ImgProps> = ({
   alt = "",
   className,
+  customLoader,
+  fill = true,
   height,
-  isFill = true,
-  isPriority,
   isSvg,
-  loader,
   quality = 75,
   sizes = "(min-width: 0) 100vw",
-  src,
   style,
   width,
+  ...props
 }) => {
   const imgRef = useRef<HTMLImageElement>(null);
   const [isLoading, setIsLoading] = useState(true);
 
   return (
     <div className={clsx(s.img, isSvg && s.img_svg, className)} style={style}>
-      {src && (
-        <>
-          {isLoading && loader}
-          <Image
-            alt={alt}
-            priority={isPriority}
-            quality={quality}
-            src={src}
-            {...(width && height
-              ? {
-                  height: height * 2,
-                  width: width * 2,
-                }
-              : {
-                  fill: isFill,
-                  sizes,
-                })}
-            ref={imgRef}
-            onLoad={() => {
-              setIsLoading(false);
-            }}
-          />
-        </>
-      )}
+      {isLoading && customLoader}
+      <Image
+        alt={alt}
+        quality={quality}
+        {...(width && height
+          ? {
+              height: +height * 2,
+              width: +width * 2,
+            }
+          : {
+              fill,
+              sizes,
+            })}
+        ref={imgRef}
+        onLoad={() => {
+          setIsLoading(false);
+        }}
+        {...props}
+      />
     </div>
   );
 };
