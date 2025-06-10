9622bdce54065c8ef6cf5222656d861332d11175
Maksym Davydenko <maksydenko@gmail.com>
2025-06-10 21:26:24 +0300

    Add Autocomplete component and update index export

--- front/src/shared/ui/field/index.ts
+++ front/src/shared/ui/field/index.ts
@@ -1,4 +1,4 @@
-export * from './Autocomplete1/Autocomplete';
+export * from './Autocomplete/Autocomplete';
 export * from './Checkbox/Checkbox';
 export * from './Field';
 export * from './Input/Input';
