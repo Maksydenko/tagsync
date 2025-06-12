1687c860f7ebae4b13729688640db9683ce81969
Maksym Davydenko <maksydenko@gmail.com>
2025-06-12 13:32:27 +0300

    Refactor test descriptions to use consistent wording

--- front/src/shared/model/utils/__tests__/formatDate.util.test.ts
+++ front/src/shared/model/utils/__tests__/formatDate.util.test.ts
@@ -1,7 +1,7 @@
 import { formatDate } from '../formatDate.util';
 
 describe('formatDate', () => {
-  test('Correct value', () => {
+  it('correct value', () => {
     expect(formatDate({ date: new Date('2021-01-01T12:00:00Z') })).toBe(
       '01.01.2021'
     );
--- front/src/shared/model/utils/__tests__/transformPxToRem.util.test.ts
+++ front/src/shared/model/utils/__tests__/transformPxToRem.util.test.ts
@@ -3,11 +3,11 @@ import { FontSize } from '@/shared/config';
 import { transformPxToRem } from '../transformPxToRem.util';
 
 describe('transformPxToRem', () => {
-  test('Correct value', () => {
+  it('correct value', () => {
     expect(transformPxToRem(10)).toBe(`${10 / FontSize.Default}rem`);
   });
 
-  test('Incorrect value', () => {
+  it('incorrect value', () => {
     expect(transformPxToRem(NaN)).toBe(null);
   });
 });
