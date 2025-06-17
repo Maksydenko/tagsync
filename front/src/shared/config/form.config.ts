7db477c4b8fd41510356794b69e9f163d281eea2
Maksym Davydenko <maksydenko@gmail.com>
2025-06-17 21:06:57 +0300

    Enhance email validation by adding TLD check and updating regex pattern

--- front/src/shared/config/form.config.ts
+++ front/src/shared/config/form.config.ts
@@ -8,7 +8,7 @@ export const formConfig = {
   },
   email: {
     max: 254,
-    pattern: /^[A-Z0-9._%+-]+@[A-Z0-9.-]+\.[A-Z]{2,4}$/i
+    pattern: /^[A-Z0-9._%+-]+@[A-Z0-9.-]+\.[A-Z]+$/i
   },
   name: {
     min: 2,
