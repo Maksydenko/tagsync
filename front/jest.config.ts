193878405373b933332ef4bc9ae978d987692b49
Maksym Davydenko <maksydenko@gmail.com>
2025-06-20 02:35:23 +0300

    Config Jest

--- front/jest.config.ts
+++ front/jest.config.ts
@@ -1,21 +1,45 @@
-import nextJest from 'next/jest.js';
-import type { Config } from 'jest';
-
-const createJestConfig = nextJest({
-  // Provide the path to your Next.js app to load next.config.js and .env files in your test environment
-  dir: './'
-});
-
-// Add any custom config to be passed to Jest
-const config: Config = {
-  coverageProvider: 'v8',
+module.exports = {
+  collectCoverageFrom: [
+    '**/*.{js,jsx,ts,tsx}',
+    '!**/*.d.ts',
+    '!**/node_modules/**'
+  ],
   moduleNameMapper: {
-    '^@/(.*)$': '<rootDir>/src/$1'
-  },
-  // Add more setup options before each test is run
-  setupFilesAfterEnv: ['./jest.setup.ts'],
-  testEnvironment: 'jsdom'
-};
+    // Handle CSS imports (without CSS modules)
+    '^.+\\.(css|sass|scss)$':
+      '<rootDir>/src/shared/config/__mocks__/style.mock.ts',
 
-// createJestConfig is exported this way to ensure that next/jest can load the Next.js config which is async
-export default createJestConfig(config);
+    // Handle image imports
+    // https://jestjs.io/docs/webpack#handling-static-assets
+    '^.+\\.(png|jpg|jpeg|gif|webp|avif|ico|bmp|svg)$':
+      '<rootDir>/src/shared/config/__mocks__/file.mock.ts',
+
+    // Handle CSS imports (with CSS modules)
+    // https://jestjs.io/docs/webpack#mocking-css-modules
+    '^.+\\.module\\.(css|sass|scss)$': 'identity-obj-proxy',
+
+    // Handle module aliases
+    '^@/(.*)$': '<rootDir>/src/$1',
+
+    // Handle Next.js specific imports
+    '^next/font/google$':
+      '<rootDir>/src/shared/config/__mocks__/nextFontGoogle.mock.ts'
+  },
+  setupFilesAfterEnv: ['<rootDir>/jest.setup.ts'],
+  testEnvironment: 'jest-environment-jsdom',
+  testPathIgnorePatterns: ['<rootDir>/node_modules/', '<rootDir>/.next/'],
+  transform: {
+    // Use babel-jest to transpile tests with the next/babel preset
+    // https://jestjs.io/docs/configuration#transform-objectstring-pathtotransformer--pathtotransformer-object
+    '^.+\\.(js|jsx|ts|tsx|mjs)$': [
+      'babel-jest',
+      {
+        presets: ['next/babel']
+      }
+    ]
+  },
+  transformIgnorePatterns: [
+    '/node_modules/(?!(next-intl|use-intl|swiper|ssr-window|dom7)/)',
+    '^.+\\.module\\.(css|sass|scss)$'
+  ]
+};
---
+++ front/src/shared/config/__mocks__/nextFontGoogle.mock.ts
@@ -0,0 +1,14 @@
+const Montserrat = () => ({
+  className: 'mocked-montserrat',
+  variable: '--mocked-montserrat'
+});
+
+const Open_Sans = () => ({
+  className: 'mocked-open-sans',
+  variable: '--mocked-open-sans'
+});
+
+module.exports = {
+  Montserrat,
+  Open_Sans
+};
